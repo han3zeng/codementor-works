c59f9e714a82aea13c740916f3c5c4e019919938
Han <allen0346@gmail.com>
2021-12-21 15:53:58 +0800

    Adjust: Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <>
-      <h1>React Starter</h1>
-      <p>This is project for Codementor</p>
+      <h1>Codementor Demo</h1>
+      <p>This is project for Codementor interview process.</p>
       <ul>
         <li><Link to="/search">Agolia Search</Link></li>
       </ul>
